4e5d80a2353e8c373d480989668dd6ea210c6f6e
Andrew Bobylev <igr-srn@mail.ru>
2022-09-30 19:44:06 +0300

    оптимизированы функции для отображения товаров

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
 const products = [
-    {id: 1, title: 'Notebook', price: 2000},
-    {id: 2, title: 'Mouse', price: 20},
-    {id: 3, title: 'Keyboard', price: 200},
-    {id: 4, title: 'Gamepad', price: 50},
+    {id: 1, title: 'Notebook', price: 2000, img: "https://socialise-uk.online/theguide-uk/_Media/shopping-2_300.jpeg"},
+    {id: 2, title: 'Mouse', price: 20, img: "https://socialise-uk.online/theguide-uk/_Media/shopping-2_300.jpeg"},
+    {id: 3, title: 'Keyboard', price: 200, img: "https://socialise-uk.online/theguide-uk/_Media/shopping-2_300.jpeg"},
+    {id: 4, title: 'Gamepad', price: 50, img: "https://socialise-uk.online/theguide-uk/_Media/shopping-2_300.jpeg"},
 ];
 //Функция для формирования верстки каждого товара
 //Добавить в выводе изображение
-const renderProduct = (title, price) => {
+const renderProduct = ( prod) => {
     return `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
+                <img src=${prod.img}>
+                <h3>${prod.title}</h3>
+                <p>${prod.price}</p>
                 <button class="buy-btn">Купить</button>
             </div>`
 };
 const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item.title,item.price));
+    const productsList = list.map(item => renderProduct(item));
     console.log(productsList);
     document.querySelector('.products').innerHTML = productsList;
 };
